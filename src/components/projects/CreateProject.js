f983f7864187b29705aebaea10eda1a7e75df41b
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-02-23 17:43:54 +0900

    create project page added

---
+++ src/components/projects/CreateProject.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-undef */
+import React, { Component } from 'react'
+
+class CreateProject extends Component {
+  state = {
+    title: '',
+    content: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  render () {
+    return (
+      <div>
+        <div className="container">
+          <form onSubmit={this.handleSubmit} className="white">
+            <h5 className="grey-text text-darken-3">Create Project</h5>
+            <div className="input-field">
+              <label htmlFor="title">title</label>
+              <input type="text" id="title" onChange={this.handleChange} />
+            </div>
+            <div className="input-field">
+              <label htmlFor="content">content</label>
+              <textarea className="materialize-textarea" id="content" cols="30" rows="10" onChange={this.handleChange}></textarea>
+            </div>
+
+            <div>
+              <button className="btn pink-lighten-1 z-depth-0">Create Project</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CreateProject
